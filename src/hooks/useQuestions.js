bdd6f9d8af3dd23bf41254ff9e44a42c993cd16c
anasansari <ansarianas1897@gmail.com>
2024-02-09 22:25:31 +0530

    Hooks in action

---
+++ src/hooks/useQuestions.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export default function useQuestions(dispatch) {
+  useEffect(
+    function () {
+      async function fetchQuestions() {
+        try {
+          const res = await fetch("http://localhost:8000/questions");
+          const data = await res.json();
+          dispatch({ type: "fetchedData", payload: data });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      fetchQuestions();
+    },
+    [dispatch]
+  );
+}
